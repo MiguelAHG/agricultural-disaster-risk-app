25237ee7d32cbcabdd4a98bad0cf0125fd5ae313
Miguel Antonio H. Germar <migs.germar@gmail.com>
2022-03-18 19:07:02 +0800

    Add code to save geodata as GPKG

--- convert_geodata.py
+++ convert_geodata.py
@@ -1,10 +1,14 @@
+#%%
+# Import packages
 import pandas as pd
 import numpy as np
 import geopandas as gpd
 import json
 from pytopojson import topology
 
-# Open geodata
+#%%
+# Open geodata and convert geojson to topojson
+# This is necessary for an Altair map
 gdf = gpd.read_file("geodata/gadm36_PHL.gpkg")
 
 gdf = (
@@ -19,8 +23,6 @@ gdf["NAME_3"] = (
     .str.replace("ñ", "n", regex = False) # Use regular letter n
 )
 
-# Convert geojson to topojson
-# This is necessary for the map
 geo_str = gdf.to_json()
 geo_dct = json.loads(geo_str)
 
@@ -29,3 +31,17 @@ topojson = tpg({"barangay_geodata": geo_dct})
 
 with open("./geodata/barangay_topojson.json", "w") as f:
     json.dump(topojson, f, indent = 4)
+
+#%%
+# Open data again but save as GPKG
+# Do not edit barangay names' spelling
+
+gdf2 = gpd.read_file("geodata/gadm36_PHL.gpkg")
+
+gdf2 = (
+    gdf2
+    .loc[gdf2["NAME_2"] == "Butuan City"] # Get all Butuan City barangays
+)
+
+# Save the Butuan City geodata to a GPKG file
+gdf2.to_file("./geodata/gadm_butuan_city_barangays.gpkg")
